b4f276486c141123e00ee7bdf3b6a946abc4c54b
John Aitchison <KayakinKoder@users.noreply.github.com>
2025-03-11 03:54:23 +0000

    only run startup scripts once

--- build/0init.sh
+++ build/0init.sh
@@ -1,19 +1,32 @@
 # note: if you're using this devcontainer frequently, most if not all of this could be moved to a prebuild step
 # rather than this which occurs when the devcontainer first spins up.
 
-sudo apt-get update
+started=$(cat /workspaces/devcontainer/build/started.json | jq -r ".started")
 
-# node is installed in the image, here we use nvm to set our version
-# importantly, nvm has to be sorced, not called. thus the . notation. also importantly, we must set our current version to the default
-. /usr/local/share/nvm/nvm.sh && nvm install --lts && nvm use --lts && nvm alias default node
+if [[ $started == *"0"* ]]
+  then
+    sudo apt-get update
 
-# udpate npm. note this needs to come after nvm node install, since npm is updated more frequently than node
-npm install npm@latest -g
+    # node is installed in the image, here we use nvm to set our version
+    # importantly, nvm has to be sorced, not called. thus the . notation. also importantly, we must set our current version to the default
+    . /usr/local/share/nvm/nvm.sh && nvm install --lts && nvm use --lts && nvm alias default node
 
-# install the neon.tech cli for postgres bliss
-sudo npm i -g neonctl
+    # udpate npm. note this needs to come after nvm node install, since npm is updated more frequently than node
+    npm install npm@latest -g
+
+    # install the neon.tech cli for postgres bliss
+    sudo npm i -g neonctl
+
+    # update our json to indicate that we've successfully completed initial startup, so that next start we won't
+    # rerun all of this
+    startup=$(jq --null-input '{"started":1}')
+    echo $startup > /workspaces/devcontainer/build/started.json
+  else
+    # nothing yet
+fi
 
 # let the user know what versions are installed
+printf "------------------- \n\n"
 printf "npm version: %s\n" $(npm --version)
 printf "node version: %s\n" $(node --version)
 printf "neon cli version: %s\n" $(neonctl --version)
