1886f0c3d362fea5bbe86728687188ecd3efd895
John Aitchison <KayakinKoder@users.noreply.github.com>
2025-03-11 04:17:40 +0000

    more init setup

--- build/0init.sh
+++ build/0init.sh
@@ -1,10 +1,13 @@
 # note: if you're using this devcontainer frequently, most if not all of this could be moved to a prebuild step
 # rather than this which occurs when the devcontainer first spins up.
 
+# determine if this is the first start of the container, and if it is set things up!
 started=$(cat /workspaces/devcontainer/build/started.json | jq -r ".started")
-
-if [[ $started == *"0"* ]]
+if [[ $started == *"1"* ]]
   then
+    # nothing needed yet
+    printf ""
+  else
     sudo apt-get update
 
     # node is installed in the image, here we use nvm to set our version
@@ -17,16 +20,19 @@ if [[ $started == *"0"* ]]
     # install the neon.tech cli for postgres bliss
     sudo npm i -g neonctl
 
+    # pulumi for ias
+    curl -fsSL https://get.pulumi.com | sh
+
     # update our json to indicate that we've successfully completed initial startup, so that next start we won't
     # rerun all of this
     startup=$(jq --null-input '{"started":1}')
     echo $startup > /workspaces/devcontainer/build/started.json
-  else
-    # nothing yet
 fi
 
 # let the user know what versions are installed
-printf "------------------- \n\n"
+printf "___________________ \n\n"
+# note that pulumi isnt in path during initial startup, so this wont work in that specific case
+printf "pulumi version: %s\n" $(pulumi version)
 printf "npm version: %s\n" $(npm --version)
 printf "node version: %s\n" $(node --version)
 printf "neon cli version: %s\n" $(neonctl --version)
