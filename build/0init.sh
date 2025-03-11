8f9abd6d3b4d739b1cf816f42d77cb95dbc34b40
John Aitchison <KayakinKoder@users.noreply.github.com>
2025-03-11 03:25:08 +0000

    install common tools

--- build/0init.sh
+++ build/0init.sh
@@ -1,13 +1,20 @@
+# note: if you're using this devcontainer frequently, most if not all of this could be moved to a prebuild step
+# rather than this which occurs when the devcontainer first spins up.
+
 sudo apt-get update
+
+# udpate npm
+npm install npm@latest -g
 
 # node is installed in the image, here we use nvm to set our version
 # importantly, nvm has to be sorced, not called. thus the . notation
 # also importantly, we must set our current version to the default
-. /usr/local/share/nvm/nvm.sh && nvm install --lts && nvm use --lts && nvm alias default --lts
+. /usr/local/share/nvm/nvm.sh && nvm install --lts && nvm use --lts && nvm alias default node
 
 # install the neon.tech cli for postgres bliss
 sudo npm i -g neonctl
 
 # let the user know what versions are installed
-node --version
-neonctl --version
+printf "npm version: %s\n" $(npm --version)
+printf "node version: %s\n" $(node --version)
+printf "neon cli version: %s\n" $(neonctl --version)
