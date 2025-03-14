c2f8401ed86777530a5dc1e000f124dd0f343c7a
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-03-14 17:43:52 +0000

    redis startup

--- tools/repo-redis.sh
+++ tools/repo-redis.sh
@@ -1,4 +1,5 @@
 # clone redis into the workspace
 cd /workspaces/tools && git clone https://github.com/KayakinCoder/redis.git
 
-# visit the repo for info on how to spin up a db
+# start things up
+source /workspaces/tools/redis/startup.sh
