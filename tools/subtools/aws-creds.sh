93a1d515b57e5bc62c08710d40085f53f57ef250
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-03-25 00:57:17 +0000

    aws creds setup

--- tools/subtools/aws-creds.sh
+++ tools/subtools/aws-creds.sh
@@ -1,1 +1,8 @@
 # iff ~/.aws/credentails doesn't exist, create it and populate with my personal aws creds from codespaces env vars
+if ! test -f ~/.aws/credentails; then
+  mkdir ~/.aws
+  cp /workspaces/devcontainer/tools/subtools/aws-creds ~/.aws
+  mv ~/.aws/aws-creds ~/.aws/credentials
+
+  # replace placeholder with github codespaces env vars
+fi
