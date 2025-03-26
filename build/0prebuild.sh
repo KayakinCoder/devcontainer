f61b1db88a176403250f02a4a107afdabfd01bff
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-03-26 01:00:22 +0000

    smooth aws sso setup

--- build/0prebuild.sh
+++ build/0prebuild.sh
@@ -1,1 +1,1 @@
-# in future if you want to prebuild, do so here
+# in future if you want to prebuild, do so here. then edit .devcontainer/devcontainer.json, uncomment onCreateCommand
--- tools/subtools/aws-creds.sh
+++ tools/subtools/aws-creds.sh
@@ -1,8 +1,11 @@
-# iff ~/.aws/credentails doesn't exist, create it and populate with my personal aws creds from codespaces env vars
+# iff aws config file doesn't exist, create it and populate with my personal aws creds from codespaces env vars
 if ! test -f ~/.aws/credentails; then
   mkdir ~/.aws
   cp /workspaces/devcontainer/tools/subtools/aws-creds ~/.aws
-  mv ~/.aws/aws-creds ~/.aws/credentials
+  mv ~/.aws/aws-creds ~/.aws/config
+
+  # replace CS_ENVVAR_AWSSSOURL with my actual aws sso endpoint. this is an GH Codespaces scoped env var
+  sed -i "s/CS_ENVVAR_AWSSSOURL/$CS_ENVVAR_AWSSSOURL/g" ~/.aws/config
 
   # replace placeholder with github codespaces env vars
 fi
