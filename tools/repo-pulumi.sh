056d3bb8a689f56392d973c198f0ac1d60e9643a
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-03-26 00:10:56 +0000

    update terraform

--- tools/repo-pulumi.sh
+++ tools/repo-pulumi.sh
@@ -3,8 +3,10 @@ source /workspaces/devcontainer/tools/subtools/aws-cli-install.sh
 
 # clone pulumi into the workspace
 cd /workspaces/tools && git clone https://github.com/KayakinCoder/pulumi.git
+cd pulumi
 
 # install the cli
 curl -fsSL https://get.pulumi.com | sh
 
 # pulumi isn't in path initially. open a new terminal and run  pulumi version  if you want to see installed version
+printf "\n\n" && aws --version
--- tools/repo-terraform.sh
+++ tools/repo-terraform.sh
@@ -2,11 +2,13 @@
 source /workspaces/devcontainer/tools/subtools/aws-cli-install.sh
 
 # download/install terraform cli
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-sudo apt update
-sudo apt-get install terraform
-
-printf "\n\n" && terraform --version
+cd /workspaces/devcontainer
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
 
 # clone repo into the workspace
 cd /workspaces/tools && git clone https://github.com/KayakinCoder/terraform.git
+cd terraform
+
+printf "\n\n" && terraform --version && aws --version
