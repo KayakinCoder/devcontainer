fc4312a59db2f1c75df3e83f3231012db8e4ff46
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-03-26 21:54:34 +0000

    cleanup

--- tools/subtools/aws-creds.sh
+++ tools/subtools/aws-creds.sh
@@ -1,5 +1,6 @@
 # iff aws config file doesn't exist, create it and populate with my personal aws creds from codespaces env vars
 if ! test -f ~/.aws/config; then
+  mkdir ~/.aws
   touch ~/.aws/config
 
   echo "$CS_ENVVAR_AWSCONFIG" > ~/.aws/config
