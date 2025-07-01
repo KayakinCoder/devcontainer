78a60ba92e650c064ba1c7617a242d2150f1fb9d
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-07-01 05:15:30 +0000

    repo sh

---
+++ tools/repo_private-main.sh
@@ -0,0 +1,6 @@
+cd /workspaces/tools && git clone https://github.com/KayakinCoder/be-private.git
+cd /workspaces/tools && git clone https://github.com/KayakinCoder/pulumi-private.git
+cd /workspaces/tools && git clone https://github.com/KayakinCoder/uis-private.git
+cd /workspaces/tools && git clone https://github.com/KayakinCoder/wcl-private.git
+cd /workspaces/tools && git clone https://github.com/KayakinCoder/devcontainer-private.git
+cd /workspaces/tools && git clone https://github.com/KayakinCoder/dbt-bigquery-private.git
